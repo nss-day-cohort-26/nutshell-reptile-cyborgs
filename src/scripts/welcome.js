110fc029da1bc9caf4d4457a703be6b1c34e2fca
Shuaib Sajid <shusajid@gmail.com>
2018-06-29 15:25:02 -0500

    added s to exports, cleaned up index.html

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,23 +1,30 @@
 // added Jquery library
-const $ = require("./jquery")
+const $ = require("jquery")
 
-const buildWelcome = () => {
-    // Selects the main div class .div--container on the index
-    const divRef = document.querySelector(".div--container");
+const buildWelcomeObject = Object.create({}, {
+            buildWelcome: {
+                value: () => {
 
-    //created a div and gave it the class of div-welcome
-    const $divWelcome = $("<div>").addClass("div-Welcome");
+                    // Selects the main div class .div--container on the index
+                    const divRef = document.querySelector(".div--container");
+                    console.log("welcome")
+                    //created a div and gave it the class of div-welcome
+                    const $divWelcome = $("<div>").addClass("div-Welcome");
+                    const $paraRef = $("<p>")
+                    //Created elements with ID's
+                    const $h1Welcome = $("<h1>").text("Welcome");
+                    const $loginButton = $("<button>").text("Login");
+                    const $registerButton = $("<button>").text("Sign Up");
 
-    //Created elements with ID's
-    const $h1Welcome = $("<h1>").text("Welcome").id("h1Welcome");
-    const $loginButton = $("<button>").text("Login").id("loginButton");
-    const $registerButton = $("<button>").text("Sign Up").id("registerButton");
+                    //Appending to DOM.
+                    $registerButton.appendTo($paraRef);
+                    $loginButton.appendTo($paraRef);
+                    $paraRef.appendTo($h1Welcome);
+                    $h1Welcome.appendTo($divWelcome);
+                    $divWelcome.appendTo(divRef);
+                }
+            }
+        })
 
-    //Appending to DOM.
-    $registerButton.appendTo($loginButton);
-    $loginButton.appendTo($h1Welcome);
-    $h1Welcome.appendTo(divRef);
-}
-
-//exports buildWecome module
-module.export = buildWelcome
+        //exports buildWelcome module
+        module.exports = buildWelcomeObject
