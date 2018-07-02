f48b7f332ce95cf3b763df6b015fbcf4d2d56084
mdroberts74 <mikedroberts88@gmail.com>
2018-07-02 11:46:11 -0500

    Added varible names to the form elements.

--- src/scripts/regform.js
+++ src/scripts/regform.js
@@ -8,19 +8,19 @@ const RegForm = Object.create({}, {
             // Selects the main div class .div--container on the index
             const divRef = document.querySelector(".divContainer");
 
-            //created a div and gave it the class of div-welcome
-            const $formDiv = $("<div>").attr("id", "regForm").appendTo($divContainer);
-
             //The Title Of The Registration Form
-            $("<h2>").attr("id", "regTitle").text("Registration Form").appendTo($regForm);
+            const $regTitle = $("<h2>").attr("id", "regTitle").text("Registration Form").appendTo($formDiv);
 
             //The form fields containing the fields and ID's
-            $("<input>").attr("id", "nameInput").attr("placeholder", "Name").appendTo($regTitle);
-            $("<input>").attr("id", "displayName").attr("placeholder", "Display Name").appendTo($nameInput);
-            $("<input>").attr("type", "email").attr("id", "regEmail").attr("placeholder", "Email").appendTo($displayName);
-            $("<input>").attr("type", "password").attr("id", "regPass").attr("placeholder", "Password").appendTo($regEmail);
-            $("<input>").attr("type", "date").attr("id", "birthDate").attr("placeholder", "Birth Date").appendTo($regPass);
-            $("<button>").attr("type", "button").text("Submit").attr("id", "regSubBtn").appendTo($birthDate);
+            const $nameInput = $("<input>").attr("id", "nameInput").attr("placeholder", "Name").appendTo($regTitle);
+            const $displayName = $("<input>").attr("id", "displayName").attr("placeholder", "Display Name").appendTo($nameInput);
+            const $regEmail = $("<input>").attr("type", "email").attr("id", "regEmail").attr("placeholder", "Email").appendTo($displayName);
+            const $regPass = $("<input>").attr("type", "password").attr("id", "regPass").attr("placeholder", "Password").appendTo($regEmail);
+            const $birthDate = $("<input>").attr("type", "date").attr("id", "birthDate").attr("placeholder", "Birth Date").appendTo($regPass);
+            const $regSubBtn = $("<button>").attr("type", "button").text("Submit").attr("id", "regSubBtn").appendTo($birthDate);
+
+            //created a div and gave it the class of div-welcome
+            const $formDiv = $("<div>").appendTo($divContainer);
             console.log(regForm);
 
 
@@ -29,4 +29,4 @@ const RegForm = Object.create({}, {
 })
 
 //exports buildWelcome module
-module.exports = buildRegForm
+module.exports = regForm;
