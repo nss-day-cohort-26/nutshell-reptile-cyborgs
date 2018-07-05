5c079ca528fa6d5e586184e8472067c35bd5ec4b
mdroberts74 <mikedroberts88@gmail.com>
2018-07-05 14:01:58 -0500

    Customized NAV bar

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -11,23 +11,29 @@ const userHome = Object.create({}, {
         value: () => {
 
             navBar.innerHTML = `
-<a class="navbar-brand" href="#">Navbar</a>
+<a class="navbar-RC" href="#">Reptile Cyborgs</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
 <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Friends List <span class="sr-only">(current)</span></a>
 </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">News</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Tasks</a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Friends Tasks</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Chat/a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Events</a>
       </li>
     </ul>
   </div>
