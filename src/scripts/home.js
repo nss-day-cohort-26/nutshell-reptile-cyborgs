b220d7fc0de0d12d9df9a16854332455d339ffe6
Aaron Miller <millerac333@gmail.com>
2018-07-05 11:00:34 -0500

    added to home.js

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -4,6 +4,7 @@
 const $ = require("jquery")
 const databaseMethod = require("./databaseMethods")
 
+//User Header and seach bar&button//
 const userHome = Object.create({}, {
     buildUserHome: {
         value: () => {
@@ -11,18 +12,24 @@ const userHome = Object.create({}, {
             const divRef = document.querySelector(".div--container");
 
             const $userContainerDiv = $("<div>").attr("id", "userHeaderContainer");
+
             const $userNameTitle = $("<h1>").attr("id", "userNameTitle").text({}).appendTo($userContainerDiv);
 
-            const $labelSearchBar = $("<label>").text("Search:")
+            const $labelSearchBar = $("<label>").text("Search:");
             const $searchInput = $("<input>").attr("id", "searchInput").attr("placeholder", "Search").appendTo($labelSearchBar);
             $labelSearchBar.appendTo($userContainerDiv);
 
-            const $searchButton = $("<button>").attr("type", "button").text("Search").attr("id", "searchButton").appendTo($userContainerDiv)
+            const $searchButton = $("<button>").attr("type", "button").text("Search").attr("id", "searchButton").appendTo($userContainerDiv);
+
+            const $contentContainerDiv = $("<div>").attr("id", "contentContainerDiv").appendTo($userContainerDiv);
+
+            const $userContentSection = $("<section>").attr("id", "userContentSection").appendTo($userContainerDiv)
         }
     }
 })
 
-module.exports = userHome;
+
+
 
 
 
