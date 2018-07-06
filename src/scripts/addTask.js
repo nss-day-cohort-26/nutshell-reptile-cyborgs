51172c261c0e09c438c9b8ad4f73cd66d27c3ecd
Emily Venturino <ebventurino@gmail.com>
2018-07-06 15:31:04 -0500

    made Task box

---
+++ src/scripts/addTask.js
@@ -0,0 +1,21 @@
+const $ = require("jquery")
+
+const databaseMethod = require("./databaseMethods")
+
+const addTask = Object.create({}, {
+    addTask: {
+        value: () => {
+            const divRef = document.querySelector(".div--container");
+            const $divTask = $("<div>").attr("id", "taskIdPlaceholder").text("Task").appendTo(divRef)
+            const $paraTask = $("<p>").appendTo($divTask)
+            const $taskTitleLabel = $("<label>").attr("for", "artTitle").text("Task Name").appendTo($paraTask)
+            const $taskTitleInput = $("<input>").attr("id", "artTitle").appendTo($taskTitleLabel)
+
+            const $taskCompletion = $("<label>").attr("for", "taskToComplete").text("Completion Date").appendTo($paraTask)
+            const $taskContentInput = $(":checkbox").attr("id", "taskToComplete").appendTo($taskContentLabel)
+
+        }
+
+    } //end of addArticle Method
+}) //end of object
+module.exports = addTask;
