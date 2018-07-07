d10b362c85c2514aec02b9a1c066f876031515f7
Aaron Miller <millerac333@gmail.com>
2018-07-06 23:52:10 -0500

    added friends database methods to databaseMethods.js

--- src/scripts/databaseMethods.js
+++ src/scripts/databaseMethods.js
@@ -2,7 +2,6 @@
 
 const $ = require("jquery");
 
-
 const databaseMethods = Object.create({}, {
     postUser: {
         value: (user) => {
@@ -10,7 +9,6 @@ const databaseMethods = Object.create({}, {
                 url: "http://localhost:3000/users",
                 method: "POST",
                 data: user
-
             })
         }
     },
@@ -27,7 +25,6 @@ const databaseMethods = Object.create({}, {
                 url: "http://localhost:3000/news",
                 method: "POST",
                 data: articles
-
             })
         }
     },
@@ -37,6 +34,7 @@ const databaseMethods = Object.create({}, {
             return $.ajax("http://localhost:3000/news")
         }
     },
+
     getAnArticle: {
         value: (id) => {
             return $.ajax(`http://localhost:3000/news/${id}`)
@@ -53,27 +51,58 @@ const databaseMethods = Object.create({}, {
     },
 
     postTask: {
-        valule: (task) => {
+        value: (task) => {
             return $.ajax({
                 url: "http://localhost:3000/tasks/",
                 method: "POST",
                 data: tasks
             })
-
         }
-
-
     },
+
     getAllTasks: {
         value: () => {
             return $.ajax("http://localhost:3000/tasks")
         }
     },
+
     putTask: {
         value: (id) => {
             $.ajax({
                 url: `http://localhost:3000/tasks/${id}`,
                 method: "PUT"
+            })
+        }
+    },
+
+    //--- <a class="nav-link" href="#">Friends List<span class="sr-only">(current)</span></a>---FIX CLASS IF NECESSARY---//
+    postFriend: {
+        value: (friend) => {
+            return $.ajax({
+                url: "http://localhost:3000/friends",
+                method: "POST",
+                data: friend
+            })
+        }
+    },
+
+    getAllFriends: {
+        value: () => {
+            return $.ajax("http://localhost:3000/friends")
+        }
+    },
+
+    getAnArticle: {
+        value: (id) => {
+            return $.ajax(`http://localhost:3000/friends/${id}`)
+        }
+    },
+
+    deleteFriends: {
+        value: (id) => {
+            return $.ajax({
+                url: `http://localhost:3000/friends/${id}`,
+                method: "DELETE"
             })
         }
     }
