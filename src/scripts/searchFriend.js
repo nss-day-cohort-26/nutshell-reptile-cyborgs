d3c8dbb97d4e5d7b66a6135230223f12303c8d12
Aaron Miller <millerac333@gmail.com>
2018-07-09 13:57:25 -0500

    blah

---
+++ src/scripts/searchFriend.js
@@ -0,0 +1,17 @@
+// const $ = require("jquery");
+// const databaseMethods = require("./databaseMethods")
+
+// const searchFriend = Object.create({}, {
+//     friendSearch: {
+//         value: () => {
+//             const $searchFriendValue = document.getElementById("friend-user-name").value;
+//             databaseMethod.searchUsers("friend-user-name");
+
+
+//             }
+
+          
+//         }
+//     }
+
+// })
